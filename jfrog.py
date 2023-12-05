501ca366077855cb0d36013fe7a2126219c1dfbd
Gopala Gabbita <107900800+gopalagabbita@users.noreply.github.com>
2023-12-04 21:52:34 -0500

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 
 def jfrogUpload():
-    url = "http://52.91.67.213:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.3-SNAPSHOT.jar"
+    url = "http://18.204.207.55:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.3-SNAPSHOT.jar"
     file_path = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.3-SNAPSHOT.jar"
     username = 'admin'
     password = 'Admin123'
